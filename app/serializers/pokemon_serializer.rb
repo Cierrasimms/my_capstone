7c5708b9e68dd52ba15fa119ff107bcd9c8e4aa2
CierraSimms <cierrasimms98@gmail.com>
2022-04-14 22:03:06 -0400

    added pokemon serializer

---
+++ app/serializers/pokemon_serializer.rb
@@ -0,0 +1,3 @@
+class PokemonSerializer < ActiveModel::Serializer
+  attributes :id, :name, :pokemon_type, :species, :height, :weight, :hp, :attack, :defense, :spec_attack, :spec_defense, :speed, :total
+end
